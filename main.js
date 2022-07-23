56343e9ae38f904f25a504a45c678362c227ccfc
mykolassl <emykolas@gmail.com>
2022-07-23 13:55:07 +0300

    Add functionality to the game

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const Player = (name, sign) => {
+    const getPlayerName = () => name;
+    const getPlayerSign = () => sign;
+
+    return { getPlayerName, getPlayerSign };
+};
+
 const gameBoard = (() => {
     const winningConditions = [
         [0, 1, 2],
@@ -12,9 +19,12 @@ const gameBoard = (() => {
     let gameState = ['', '', '',
                      '', '', '', 
                      '', '', '' ];
-
-    const placeSign = (sign, row, col) => {
-
+                     
+    const getGameState = () => gameState;
+    const getWinningConditions = () => winningConditions;
+                     
+    const placeSign = (sign, cellIndex) => {
+        gameState[cellIndex] = sign;
     };
 
     const checkWin = () => {
@@ -30,25 +40,141 @@ const gameBoard = (() => {
         return false;
     };
 
-    return { placeSign, checkWin };
+    const isCellEmpty = (index) => gameState[index] === '' ? true : false;
+
+    const checkTie = () => {
+        for(const cell of gameState) {
+            if(cell === '') return false;
+        }
+
+        return true;
+    };
+
+    const clearBoard = () => {
+        for(let i = 0; i < gameState.length; i++) {
+            gameState[i] = '';
+        }
+    };
+
+    return { getGameState, getWinningConditions, placeSign, isCellEmpty, checkWin, checkTie, clearBoard };
 })();
 
 const displayController = (() => {
+    const playerOne = Player('Player1', 'X');
+    const playerTwo = Player('Player2', 'O');
+    let currentPlayer = {};
+    
+    const startGame = () => {
+        const startButton = document.querySelector('#btn-start');
+        startButton.disabled = false;
+
+        startButton.addEventListener('click', () => {
+            startButton.disabled = true;
+            setupBoard();
+        });
+    };
+
     const setupBoard = () => {
+        const display = document.querySelector('.game-status-container > h2');
+        const cells = document.querySelectorAll('.cell');
+        
+        display.innerText = 'X starts the game!';
+        
+        gameBoard.clearBoard();
+        clearBoard();
 
+        currentPlayer = playerOne;
+
+        cells.forEach((cell) => {
+            cell.addEventListener('click', handleCellClick);
+        });
     };
 
-    const placeSign = () => {
+    const clearBoard = () => {
+        const cells = document.querySelectorAll('.cell');
 
+        cells.forEach((cell) => {
+            cell.innerText = '';
+            cell.classList.remove('winning-cell');
+        });
     };
 
-    const cleanBoard = () => {
+    const handleCellClick = (event) => {
+        placeSign(event);
+        const display = document.querySelector('.game-status-container > h2');
+        const hasPlayerWon = gameBoard.checkWin();
+        const isTie = gameBoard.checkTie();
 
+        if(!hasPlayerWon) {
+            switchTurns(playerOne, playerTwo);
+            display.innerText = `${currentPlayer.getPlayerSign()}'s turn`
+        } else {
+            display.innerText = `${currentPlayer.getPlayerSign()} won the game!`;
+            colorWinningCells();
+            removeCellListeners();
+            showPlayAgainModal();
+            return;
+        }
+
+        if(isTie) {
+            display.innerText = `It's a tie!`;
+            removeCellListeners();
+            showPlayAgainModal();
+        }
     };
+
+    const placeSign = (e) => {
+        const cellIndex = e.target.dataset.cellId;
+        const sign = currentPlayer.getPlayerSign();
+        const isValidCell = gameBoard.isCellEmpty(cellIndex);
+
+        if(!isValidCell) return;
+
+        e.target.innerText = sign;
+        gameBoard.placeSign(sign, cellIndex);
+    };
+
+    const switchTurns = (p1, p2) => {
+        if(currentPlayer.getPlayerName() === p1.getPlayerName()) currentPlayer = p2;
+        else currentPlayer = p1;
+    };
+
+    const removeCellListeners = () => {
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach((cell) => {
+            cell.removeEventListener('click', handleCellClick);
+        })
+    };
+
+    const colorWinningCells = () => {
+        const cells = document.querySelectorAll('.cell');
+        const winningConditions = gameBoard.getWinningConditions();
+        const board = gameBoard.getGameState();
+        
+        for(const condition of winningConditions) {
+            const cellOne = board[condition[0]];
+            const cellTwo = board[condition[1]];
+            const cellThree = board[condition[2]];
+
+            if(cellOne === cellTwo && cellTwo === cellThree) {
+                cells[condition[0]].classList.add('winning-cell');
+                cells[condition[1]].classList.add('winning-cell');
+                cells[condition[2]].classList.add('winning-cell');
+                return;
+            }
+        }
+    };
+
+    const showPlayAgainModal = () => {
+        const againButton = document.querySelector('#btn-again');
+        againButton.disabled = false;
+        againButton.addEventListener('click', () => {
+            setupBoard();
+            againButton.disabled = true;
+        });
+    };
+
+    return { startGame };
 })();
 
-const Player = (name, sign) => {
-    const getPlayerName = () => name;
-};
-
-console.log(gameBoard.checkWin());
+displayController.startGame();
